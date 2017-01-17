7601e8f12c8af05f46a335b39b5051e684dfebfb
Pablo Morales <pmorales@sophilabs.com>
2017-01-17 16:04:03 -0300

    namming convention fixed

--- tw-imp.py
+++ tw-imp.py
@@ -1,3 +1,4 @@
+import pdb
 import sys
 import twitter
 from datetime import datetime
@@ -12,19 +13,20 @@ def main():
 	                      access_token_key='817160799819010048-PPnDnUu2uZqmuXMgIgfMGZ4oZr3SXqm',
 	                      access_token_secret='Uv4uWr5lqCnaA7zjzcIzkIU87KZz31lJJ8cyYUn315e8E')
 
-	usersToImpersonate = {"TheEllenShow" : [], "LEIitalia" : []}
-	startTime = datetime.now()
-	impersonatedStatuses = []
-	refreshRate = 10 #minutes
+	pdb.set_trace()
+	users_to_impersonate = {"TheEllenShow" : [], "LEIitalia" : []}
+	start_time = datetime.now()
+	impersonated_statuses = []
+	refresh_rate = 10 #minutes
 
 	while True:
-		sleep(refreshRate * 60)
-		for user, impersonated_statuses in usersToImpersonate:
+		sleep(refresh_rate * 60)
+		for user, impersonated_statuses in users_to_impersonate:
 			since_status = None
 			if len(impersonated_statuses) != 0:
 				since_status = impersonated_statuses[-1]
-			userTimeline = api.GetUserTimeline(screen_name=user, since_id=since_status)
-			for status in userTimeline:
+			user_timeline = api.GetUserTimeline(screen_name=user, since_id=since_status)
+			for status in user_timeline:
 				if is_new(status=status) and not was_impersonated(status=status):
 					impersonate(status=status)		
 	
@@ -34,15 +36,15 @@ def str_to_datetime(dt_str):
 	return datetime.strptime(dt_str, '%a %b %d %H:%M:%S %z %Y')
 
 def is_new(status):
-	return str_to_datetime(status.created_at) > startTime
+	return str_to_datetime(status.created_at) > start_time
 	
 def was_impersonated(status):
-	return status.id in impersonatedStatuses
+	return status.id in impersonated_statuses
 	
 def impersonate(status):
 	try:
 		result = api.PostUpdates(status)
-		impersonatedStatuses.append(status.id);
+		impersonated_statuses.append(status.id);
 		print("Status Impersonated :", status)
 		print()
 	except twitter.error.TwitterError as te:
