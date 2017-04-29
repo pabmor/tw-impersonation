fc49134dcb331a354169926c9f098aa9851514bd
Pablo Morales <pmorales@sophilabs.com>
2017-04-28 23:45:22 -0300

    Created configuration files. To improve flexibility

---
+++ configuration/values.py
@@ -0,0 +1,5 @@
+#Targets to impersonate.
+targets = ['katyperry', 'rihanna', 'ladygaga']
+
+#Seconds between checking users timeline - if too low can exceed the tw api rate limit
+refresh_rate = 120
