aa866665d73c053d6baeecb6eeb979cab2a02405
Pablo Morales <pmorales@sophilabs.com>
2017-04-28 23:58:28 -0300

    Yes

--- configuration/values.py
+++
@@ -1,5 +0,0 @@
-#Targets to impersonate.
-targets = ['katyperry', 'rihanna', 'ladygaga']
-
-#Seconds between checking users timeline - if too low can exceed the tw api rate limit
-refresh_rate = 120
